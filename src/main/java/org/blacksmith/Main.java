024fd3446d2d6fd1cc214cfabc2a16aff2dc076d
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-13 19:08:01 +0530

    Going to the structure in intellij

--- src/main/java/org/blacksmith/Main.java
+++ src/main/java/org/blacksmith/Main.java
@@ -1,6 +1,17 @@
 package org.blacksmith;
 
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
 public class Main {
-    public static void main(String[] args) {
+//    private static Robot robot = null;
+    public static void main(String[] args) throws AWTException {
+        Robot robot = new Robot();
+        robot.mouseMove(25,240);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        robot.delay(50);
     }
+
 }
