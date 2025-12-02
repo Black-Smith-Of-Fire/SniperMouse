0ecda9d097baeb08163a21439f793e1fe378eb30
blacksmithoffire <blacksmithoffire.com>
2025-12-02 14:18:44 +0530

    Lost progress

--- src/main/java/org/blacksmith/TransparentWindow.java
+++ src/main/java/org/blacksmith/TransparentWindow.java
@@ -2,56 +2,56 @@ package org.blacksmith;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 
-public class TransparentWindow extends JFrame {
 
+public class TransparentWindow {
+
+    public static void click(int x, int y) throws AWTException{
+        Robot bot = new Robot();
+        bot.mouseMove(x, y);
+        bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+//            click();
+        }
+    }
     public static void main(String[] args) {
-        GraphicsEnvironment graphics =
-                GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice device = graphics.getDefaultScreenDevice();
-//         try block
-        try {
+        JFrame frame = new JFrame();
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+//        frame.setResizable(false);
+        frame.setUndecorated(true);
+        frame.setOpacity(0.1f);
+        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        Rectangle bounds = env.getMaximumWindowBounds();
 
-            // create a window
-            JWindow w = new JWindow();
+        JPanel p = new JPanel() {
+            // paint the panel
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setStroke(new BasicStroke(3));
+                g2.setColor(Color.RED);
+                g2.drawLine(bounds.width/2,0,bounds.width/2,bounds.height);
+                g2.drawLine(0,bounds.height/2,bounds.width,bounds.height/2);
+            }
+        };
 
-            // set a transparent  background of the window
-            w.setBackground(new Color(0,0,0,0));
+        frame.add(p);
+        frame.setVisible(true);
 
-            // create a panel
-            JPanel p = new JPanel() {
-
-                // paint the panel
-                public void paintComponent(Graphics g) {
-                    g.setColor(Color.RED);
-                    g.drawLine(100,10,100,1000);
-                }
-            };
-
-            // add panel
-            w.add(p);
-
-            // set the location
-
-            w.setLocation(0, 0);
-
-            // set the size of the window
-            w.setSize(900, 900);
-
-            // set the visibility of the window
-            w.setVisible(true);
+        // clicking the button
 
 
-            device.setFullScreenWindow(w);
-        }
-        // catch any exception
-        catch (Exception e) {
-
-            // show the error
-            System.err.println(e.getMessage());
-        }
     }
 }
