9bd11760f6ae7b59bf7680bc82e077aee96d9b50
blacksmithoffire <blacksmithoffire.com>
2025-12-04 17:04:40 +0530

    Got the jframe moving

--- src/main/java/org/blacksmith/TransparentWindow.java
+++ src/main/java/org/blacksmith/TransparentWindow.java
@@ -3,84 +3,129 @@ package org.blacksmith;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 
 
-public class TransparentWindow {
+public class TransparentWindow extends JFrame implements Runnable, KeyListener {
+    Thread t;
+    int x = 0;
+    int y = 0;
+    int originX = 0;
+    int originY = 0;
+    boolean vim = true;
 
-    public static void click(int x, int y) throws AWTException{
-        Robot bot = new Robot();
-        bot.mouseMove(x, y);
-        bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-    }
+    public TransparentWindow() {
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-//        frame.setResizable(false);
-        frame.setUndecorated(true);
-        frame.setOpacity(0.1f);
-        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        Rectangle bounds = env.getMaximumWindowBounds();
-        System.out.println(bounds.width);
+        // Set frame properties
+        setTitle("Animated JFrame");
+        setSize(500,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        // Setting the shape of the frame to circle
+        setShape(new Ellipse2D.Double(0,0,100,100));
+
+        // Set background for the frame's content pane
+        getContentPane().setBackground(Color.white);
+
+        // Set opacity, don't make it opaque, that's my wish!
+        setOpacity(0.5f);
 
         JPanel p = new JPanel() {
             // paint the panel
             public void paintComponent(Graphics g) {
-               /*
-                                     0
-                                     |  +
-                                     |  +
-              ++++++++ width/2 ++++++|  height/2
-                                     |  +
-                                     |  +
-             0 ______________________|________________________
-                                     |
-                                     |
-                                     |
-                                     |
-                                     |
-                                     |
-               */
+
                 super.paintComponent(g);
                 Graphics2D g2 = (Graphics2D) g;
-                g2.setStroke(new BasicStroke(3));
-                g2.setColor(Color.RED);
-                //                  start_X,  start|Y, end_X, end|Y
-                // level 1
-//                g2.drawLine(bounds.width/2,0,bounds.width/2,bounds.height); // vertical long line
-//                g2.drawLine(0,bounds.height/2,bounds.width,bounds.height/2);// horizontal long line
-//
-//                // level 2
-//                g2.setColor(new Color(0,100,0));
-//                g2.drawLine(bounds.width/4,0,bounds.width/4,bounds.height/2); // vertical sub line
-//                g2.drawLine(0,bounds.height/4,bounds.width/2,bounds.height/4);// horizontal sub line
-//
-//                // level 3
-                g2.setColor(new Color(0,0,0));
-//                g2.drawLine(bounds.width/8,0,bounds.width/8,bounds.height/4); // vertical sub line
-//                g2.drawLine(0,bounds.height/12,bounds.width/4,bounds.height/12);// horizontal sub line
-//                g2.drawLine(bounds.width/16,0,bounds.width/16,bounds.height/4); // vertical sub line
-//                g2.drawLine(0,2 * bounds.height/12,bounds.width/4,2 * bounds.height/12);// horizontal sub line
-//                g2.drawLine(3 * bounds.width/16,0,3* bounds.width/16,bounds.height/4); // vertical sub line
-//                g2.drawLine(bounds.width/12,0,bounds.width/12,bounds.height/4); // vertical sub line
 
-                g2.drawOval(bounds.width/2,bounds.height/2, 100,100);
-                g2.drawOval(bounds.width/2 + 10,bounds.height/2 + 10, 80,80);
-                g2.drawLine(bounds.width/2 + 50,bounds.height/2 + 10, bounds.width/2 + 50,bounds.height/2 + 90); // vertical sub line
-                g2.drawLine(bounds.width/2 + 10,bounds.height/2 + 50, bounds.width/2 + 90,bounds.height/2 + 50); // vertical sub line
+                g2.setColor(new Color(0,0,0));
+
+                g2.drawOval(10, 10,80,80);
+                g2.drawLine(50, 0, 50,100); // vertical line
+                g2.drawLine(0, 50, 100,50); // horizontal line
 
             }
         };
 
-        frame.add(p);
-        frame.setVisible(true);
+        add(p);
 
-        // clicking the button
+        setVisible(true);
+
+        // Create a new thread, run() is written in this class (Runnable implemented)
+        t=new Thread(this);
+
+        // Start the thread
+        t.start();
+        addKeyListener(this);
+    }
+
+    public void moveFrame(){
+        originX = originX + x;
+        originY = originY + y;
+        setLocation(originX, originY);
+    }
+
+    public void run() {
+        try {
+
+//            int width=Toolkit.getDefaultToolkit().getScreenSize().width;
+
+            while (vim) {
+                moveFrame();
+                Thread.sleep(1);
+            }
+
+        }catch(Exception e){}
+
+    }
 
 
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_L) {
+            x = 1;
+        }
+        if (key == KeyEvent.VK_H) {
+            x = -1;
+        }
+        if (key == KeyEvent.VK_J) {
+            y = 1;
+        }
+        if (key == KeyEvent.VK_K) {
+            y = -1;
+        }
+        if (key == KeyEvent.VK_I) {
+            vim = false;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_L) {
+            x = 0;
+        }
+        if (key == KeyEvent.VK_H) {
+            x = 0;
+        }
+        if (key == KeyEvent.VK_J) {
+            y = 0;
+        }
+        if (key == KeyEvent.VK_K) {
+            y = 0;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {}
+
+    public static void main(String[] args) {
+        new TransparentWindow();
     }
 }
