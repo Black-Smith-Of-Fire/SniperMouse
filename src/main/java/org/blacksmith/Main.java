8f4d94408331bfb301bda7afffc3fd34ef7543f0
blacksmithoffire <blacksmithoffire.com>
2025-12-05 22:43:14 +0530

    Driver method added here

--- src/main/java/org/blacksmith/Main.java
+++ src/main/java/org/blacksmith/Main.java
@@ -5,13 +5,9 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
 public class Main {
-//    private static Robot robot = null;
-    public static void main(String[] args) throws AWTException {
-        Robot robot = new Robot();
-        robot.mouseMove(25,240);
-        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-        robot.delay(50);
+
+    public static void main(String[] args) {
+        new SniperMouse();
     }
 
 }
