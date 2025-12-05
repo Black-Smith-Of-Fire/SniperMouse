7d9b50fdca6695778aa7f12185fe7f97fafd9ca9
blacksmithoffire <blacksmithoffire.com>
2025-12-05 20:20:15 +0530

    Sniper mouse working well

--- src/main/java/org/blacksmith/TransparentWindow.java
+++ src/main/java/org/blacksmith/TransparentWindow.java
@@ -1,4 +1,5 @@
 package org.blacksmith;
+//TODO: Use timer instead of thread
 
 import javax.swing.*;
 import java.awt.*;
@@ -21,12 +22,12 @@ public class TransparentWindow extends JFrame implements Runnable, KeyListener {
 
         // Set frame properties
         setTitle("Animated JFrame");
-        setSize(500,500);
+        setSize(100,100);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setUndecorated(true);
 
         // Setting the shape of the frame to circle
-        setShape(new Ellipse2D.Double(0,0,100,100));
+        setShape(new Ellipse2D.Double(0,0,50,50));
 
         // Set background for the frame's content pane
         getContentPane().setBackground(Color.white);
@@ -43,9 +44,9 @@ public class TransparentWindow extends JFrame implements Runnable, KeyListener {
 
                 g2.setColor(new Color(0,0,0));
 
-                g2.drawOval(10, 10,80,80);
-                g2.drawLine(50, 0, 50,100); // vertical line
-                g2.drawLine(0, 50, 100,50); // horizontal line
+                g2.drawOval(10, 10,30,30);
+                g2.drawLine(25, 0, 25,50); // vertical line
+                g2.drawLine(0, 25, 50,25); // horizontal line
 
             }
         };
@@ -82,11 +83,30 @@ public class TransparentWindow extends JFrame implements Runnable, KeyListener {
 
     }
 
+    public  void click(int x, int y) throws AWTException, InterruptedException {
+        Robot bot = new Robot();
+        bot.keyPress(KeyEvent.VK_ALT);
+        bot.keyPress(KeyEvent.VK_TAB);
+        bot.keyRelease(KeyEvent.VK_ALT);
+        bot.keyRelease(KeyEvent.VK_TAB);
+        Thread.sleep(10);
+        bot.mouseMove(x, y);
+        bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
 
         int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_F) {
+            try {
+                click(originX + 25, originY + 25);
+            } catch (AWTException | InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
 
         if (key == KeyEvent.VK_L) {
             x = 1;
